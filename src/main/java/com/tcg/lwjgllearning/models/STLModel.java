2a41e584ea31d5f3017e0e19dcf3c7d2e8bb961d
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2020-12-21 18:50:39 -0800

    STL Models

---
+++ src/main/java/com/tcg/lwjgllearning/models/STLModel.java
@@ -0,0 +1,173 @@
+package com.tcg.lwjgllearning.models;
+
+import com.tcg.lwjgllearning.graphics.Color;
+import com.tcg.lwjgllearning.graphics.ShaderProgram;
+import com.tcg.lwjgllearning.graphics.g3d.RGBMesh;
+import com.tcg.lwjgllearning.graphics.g3d.UniformColorMesh;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class STLModel {
+
+    private static final int FLOATS_PER_TRIANGLE = 9;
+    private static final int FLOATS_PER_VERTEX = 3;
+    private static final int VERTICES_PER_TRIANGLE = 3;
+    private float[] vertices;
+    private float[] normals;
+    private int[] indices;
+
+    public STLModel(String path) {
+        try (final FileInputStream inputStream = new FileInputStream(path)) {
+            final byte[] fileBytes = inputStream.readAllBytes();
+            final ByteBuffer byteBuffer = ByteBuffer.wrap(fileBytes);
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+            byte[] first5Bytes = new byte[5];
+            byteBuffer.get(0, first5Bytes);
+            final String first5Characters = new String(first5Bytes, StandardCharsets.UTF_8);
+            if (first5Characters.equals("solid")) {
+                this.readAsciiFile(fileBytes);
+            } else {
+                this.parseBinarySTL(byteBuffer);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read stl file.", e);
+        }
+    }
+
+    public static UniformColorMesh buildStlMesh(String path, ShaderProgram shaderProgram, Color color) {
+        final STLModel model = new STLModel(path);
+        return new UniformColorMesh(shaderProgram, model.vertices, model.normals, model.indices, color);
+    }
+
+    private void readAsciiFile(byte[] fileBytes) {
+        final Scanner fileScanner = new Scanner(new ByteArrayInputStream(fileBytes), StandardCharsets.UTF_8);
+        boolean done = false;
+        int triangleIndex = 0;
+        final List<Float> vertexList = new ArrayList<>();
+        final List<Float> normalList = new ArrayList<>();
+        final List<Integer> indexList = new ArrayList<>();
+        while (!done && fileScanner.hasNextLine()) {
+            final String line = fileScanner.nextLine();
+            final Scanner lineScanner = new Scanner(line);
+            final String keyWord = lineScanner.next();
+            done = keyWord.equals("endsolid");
+
+            if (keyWord.equals("facet")) {
+                this.readNextFacet(fileScanner, triangleIndex, vertexList, normalList, indexList, lineScanner);
+                triangleIndex++;
+            }
+
+        }
+        this.vertices = this.floatListToArray(vertexList);
+        this.normals = this.floatListToArray(normalList);
+        this.indices = this.intListToArray(indexList);
+    }
+
+    private void readNextFacet(Scanner fileScanner, int triangleIndex, List<Float> vertexList, List<Float> normalList, List<Integer> indexList, Scanner lineScanner) {
+        lineScanner.next();
+        final float normalX = lineScanner.nextFloat();
+        final float normalY = lineScanner.nextFloat();
+        final float normalZ = lineScanner.nextFloat();
+        fileScanner.nextLine();
+        this.readTriangleVertices(fileScanner, vertexList, normalList, normalX, normalY, normalZ);
+        fileScanner.nextLine();
+        fileScanner.nextLine();
+        final int triangleStart = triangleIndex * VERTICES_PER_TRIANGLE;
+        this.addTriangleIndices(indexList, triangleStart);
+    }
+
+    private void addTriangleIndices(List<Integer> indexList, int triangleStart) {
+        indexList.add(triangleStart);
+        indexList.add(triangleStart + 1);
+        indexList.add(triangleStart + 2);
+    }
+
+    private void readTriangleVertices(Scanner fileScanner, List<Float> vertexList, List<Float> normalList, float normalX, float normalY, float normalZ) {
+        for (int i = 0; i < 3; i++) {
+            this.readNextVertex(fileScanner, vertexList, normalList, normalX, normalY, normalZ);
+        }
+    }
+
+    private void readNextVertex(Scanner fileScanner, List<Float> vertexList, List<Float> normalList, float normalX, float normalY, float normalZ) {
+        final Scanner vertexScanner = new Scanner(fileScanner.nextLine());
+        vertexScanner.next();
+        normalList.add(normalX);
+        normalList.add(normalY);
+        normalList.add(normalZ);
+        vertexList.add(vertexScanner.nextFloat());
+        vertexList.add(vertexScanner.nextFloat());
+        vertexList.add(vertexScanner.nextFloat());
+    }
+
+
+    private int[] intListToArray(List<Integer> intList) {
+        final int[] listAsArray = new int[intList.size()];
+        for (int i = 0; i < intList.size(); i++) {
+            listAsArray[i] = intList.get(i);
+        }
+        return listAsArray;
+    }
+
+    private float[] floatListToArray(List<Float> floatList) {
+        final float[] listAsArray = new float[floatList.size()];
+        for (int i = 0; i < floatList.size(); i++) {
+            listAsArray[i] = floatList.get(i);
+        }
+        return listAsArray;
+    }
+
+    private void parseBinarySTL(ByteBuffer byteBuffer) {
+        byteBuffer.position(80);
+        final int numberOfTriangles = byteBuffer.getInt();
+        this.vertices = new float[numberOfTriangles * FLOATS_PER_TRIANGLE];
+        this.normals = new float[numberOfTriangles * FLOATS_PER_TRIANGLE];
+        this.indices = new int[(numberOfTriangles * FLOATS_PER_TRIANGLE) / VERTICES_PER_TRIANGLE];
+        this.parseAllTriangles(byteBuffer, numberOfTriangles);
+    }
+
+    private void parseAllTriangles(ByteBuffer byteBuffer, int numberOfTriangles) {
+        for (int triangleIndex = 0; triangleIndex < numberOfTriangles; triangleIndex++) {
+            this.parseNextTriangle(byteBuffer, triangleIndex);
+        }
+    }
+
+    private void parseNextTriangle(ByteBuffer byteBuffer, int triangleIndex) {
+        final float[] normalArray = this.parseTriangleNormal(byteBuffer);
+        final float[] vertices = this.parseTriangleVertices(byteBuffer);
+        final int triangleVerticesStart = triangleIndex * FLOATS_PER_TRIANGLE;
+        System.arraycopy(vertices, 0, this.vertices, triangleVerticesStart, FLOATS_PER_TRIANGLE);
+        System.arraycopy(normalArray, 0, this.normals, triangleVerticesStart, FLOATS_PER_TRIANGLE);
+        final int triangleIndexStart = triangleIndex * VERTICES_PER_TRIANGLE;
+        final int[] indexArray = {triangleIndexStart, triangleIndexStart + 1, triangleIndexStart + 2};
+        System.arraycopy(indexArray, 0, this.indices, triangleIndexStart, VERTICES_PER_TRIANGLE);
+        byteBuffer.getChar();
+    }
+
+    private float[] parseTriangleVertices(ByteBuffer byteBuffer) {
+        return new float[]{
+                byteBuffer.getFloat(), byteBuffer.getFloat(), byteBuffer.getFloat(),
+                byteBuffer.getFloat(), byteBuffer.getFloat(), byteBuffer.getFloat(),
+                byteBuffer.getFloat(), byteBuffer.getFloat(), byteBuffer.getFloat()
+        };
+    }
+
+    private float[] parseTriangleNormal(ByteBuffer byteBuffer) {
+        final float normalX = byteBuffer.getFloat();
+        final float normalY = byteBuffer.getFloat();
+        final float normalZ = byteBuffer.getFloat();
+        return new float[]{
+                normalX, normalY, normalZ,
+                normalX, normalY, normalZ,
+                normalX, normalY, normalZ
+        };
+    }
+
+}
---
+++ src/main/java/com/tcg/lwjgllearning/samplesfromgraphics/STLModelTest.java
@@ -0,0 +1,161 @@
+package com.tcg.lwjgllearning.samplesfromgraphics;
+
+import com.tcg.lwjgllearning.application.ApplicationAdapter;
+import com.tcg.lwjgllearning.application.LWJGLApplication;
+import com.tcg.lwjgllearning.graphics.Color;
+import com.tcg.lwjgllearning.graphics.ShaderProgram;
+import com.tcg.lwjgllearning.graphics.g3d.UniformColorMesh;
+import com.tcg.lwjgllearning.math.MathUtils;
+import com.tcg.lwjgllearning.math.Matrix4;
+import com.tcg.lwjgllearning.math.Quaternion;
+import com.tcg.lwjgllearning.math.Vector3;
+import com.tcg.lwjgllearning.models.STLModel;
+import com.tcg.lwjgllearning.utils.FileUtils;
+
+import java.util.stream.Stream;
+
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.opengl.GL20.glUniform3fv;
+import static org.lwjgl.opengl.GL20.glUniformMatrix4fv;
+
+public class STLModelTest extends ApplicationAdapter {
+
+    private int width;
+    private int height;
+    private Matrix4 viewMatrix;
+    private Matrix4 projectionMatrix;
+    private ShaderProgram uniformShader;
+    private ShaderProgram rgbShader;
+    private final Vector3 ambientLight = new Vector3();
+    private final Vector3 lightDirection = new Vector3();
+    private final Vector3 lightIntensity = new Vector3();
+    private UniformColorMesh binarySpaceInvader;
+    private UniformColorMesh asciiSpaceInvader;
+    private UniformColorMesh cashFloatGamePiece;
+
+    @Override
+    public void create() {
+        super.create();
+        this.ambientLight.set(0.2f, 0.3f, 0.2f);
+        this.lightDirection.set(1f, -1f, -0.25f);
+        this.lightIntensity.set(0.9f, 0.8f, 0.6f);
+
+        this.uniformShader = ShaderProgram.buildShader(
+                FileUtils.readFile("sample_shaders/lab04/vert.uniform.glsl"),
+                FileUtils.readFile("sample_shaders/lab04/frag.uniform.glsl")
+        );
+
+        this.rgbShader = ShaderProgram.buildShader(
+                FileUtils.readFile("sample_shaders/lab04/vert.rgb.glsl"),
+                FileUtils.readFile("sample_shaders/lab04/frag.rgb.glsl")
+        );
+
+        this.updateMatricesAndLighting(300, 300);
+
+
+        final Color red = Color.rgb888(0xFF0000);
+        final Color green = Color.rgb888(0x00FF00);
+        final Color blue = Color.rgb888(0x0000FF);
+        this.binarySpaceInvader = STLModel.buildStlMesh("models/space_invader_magnet_binary.stl",
+                this.uniformShader, red);
+        this.binarySpaceInvader.setScale(new Vector3(0.05f, 0.05f, 0.05f));
+        this.binarySpaceInvader.localRotate(Quaternion.ofRotation(MathUtils.PI, Vector3.x(), true));
+        this.binarySpaceInvader.translate(new Vector3(-5, 0, 0));
+
+        this.asciiSpaceInvader = STLModel.buildStlMesh("models/space_invader_magnet_ascii.stl",
+                this.uniformShader, green);
+        this.asciiSpaceInvader.setScale(new Vector3(0.05f, 0.05f, 0.05f));
+        this.asciiSpaceInvader.localRotate(Quaternion.ofRotation(MathUtils.PI, Vector3.x(), true));
+        this.asciiSpaceInvader.translate(new Vector3(5, 0, 0));
+
+        this.cashFloatGamePiece = STLModel.buildStlMesh("models/CashFlowGamePiece.stl",
+                this.uniformShader, blue);
+        this.cashFloatGamePiece.setScale(new Vector3(0.1f, 0.1f, 0.1f));
+        this.cashFloatGamePiece.localRotate(Quaternion.ofRotation(MathUtils.PI / 2, Vector3.x(), true));
+        this.cashFloatGamePiece.translate(new Vector3(-1, -1, -1));
+
+
+        glClearColor(0.75f, 0.85f, 0.8f, 1.0f);
+    }
+
+    @Override
+    public void update() {
+        final Quaternion rotation = Quaternion.ofRotation(MathUtils.PI / 180f, Vector3.y(), true);
+        this.binarySpaceInvader.localRotate(rotation);
+        this.asciiSpaceInvader.localRotate(rotation);
+        this.cashFloatGamePiece.rotateAround(Vector3.origin(), rotation, false);
+    }
+
+    @Override
+    public void draw() {
+        this.binarySpaceInvader.draw();
+        this.asciiSpaceInvader.draw();
+        this.cashFloatGamePiece.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        glViewport(0, 0, width, height);
+        this.updateMatricesAndLighting(width, height);
+    }
+
+    @Override
+    public void close() {
+        super.close();
+    }
+
+    private void updateMatricesAndLighting(int width, int height) {
+        this.updateViewMatrix();
+        this.updateProjectionMatrix(width, height);
+        this.bindMatricesAndLightingToAllShaders(this.uniformShader, this.rgbShader);
+    }
+
+    private void bindMatricesAndLightingToAllShaders(ShaderProgram... shaders) {
+        Stream.of(shaders)
+                .forEach(this::bindMatricesAndLightingToShader);
+    }
+
+    private void bindMatricesAndLightingToShader(ShaderProgram shader) {
+        shader.bind();
+
+        final int matViewUniformLocation = shader.getUniformLocation("mView");
+        glUniformMatrix4fv(matViewUniformLocation, false, this.viewMatrix.asArray());
+
+        final int matProjUniformLocation = shader.getUniformLocation("mProj");
+        glUniformMatrix4fv(matProjUniformLocation, false, this.projectionMatrix.asArray());
+
+        final int ambientLightUniformLocation = shader.getUniformLocation("ambientLight");
+        glUniform3fv(ambientLightUniformLocation, this.ambientLight.asArray());
+
+        final int lightDirectionUniformLocation = shader.getUniformLocation("lightDirection");
+        glUniform3fv(lightDirectionUniformLocation, this.lightDirection.asArray());
+
+        final int lightIntensityUniformLocation = shader.getUniformLocation("lightIntensity");
+        glUniform3fv(lightIntensityUniformLocation, this.lightIntensity.asArray());
+
+        shader.unbind();
+    }
+
+    private void updateViewMatrix() {
+        final Vector3 cameraPosition = new Vector3(0, 5, 15);
+        final Vector3 lookAtPosition = Vector3.origin();
+        final Vector3 cameraUpDirection = Vector3.y();
+        this.viewMatrix = Matrix4.view(cameraPosition, lookAtPosition, cameraUpDirection);
+    }
+
+    private void updateProjectionMatrix(int width, int height) {
+        this.width = width;
+        this.height = height;
+        final float fieldOfView = MathUtils.PI / 4;
+        final float aspect = (float) this.width / this.height;
+        final float near = 0.01f;
+        final float far = 1000f;
+        this.projectionMatrix = Matrix4.perspective(fieldOfView, aspect, near, far);
+    }
+
+    public static void main(String[] args) {
+        new LWJGLApplication(new STLModelTest());
+    }
+
+}
